e929825ca4eed936ff001f0bfad27bcc0e34a14f
saifjeb <saifjebreen5@gmail.com>
2025-10-01 15:47:19 +0300

    JS

---
+++ js/home.js
@@ -0,0 +1,54 @@
+var studentName = "Ali";
+console.log("studentName:", studentName);
+
+var isNew = true;
+console.log("isNew:", isNew);
+
+var marks = [10, 20, 30, 40, 50]
+console.log("marks:", marks);
+
+var student = { 
+    name: "Ali",
+    age: 20,
+    isNew: true 
+}
+console.log("student:", student);
+
+var y = null;
+console.log("y:", y);
+
+var ageAString = "20";
+console.log("ageAString:", ageAString); 
+
+var age = 20;
+console.log("age1:", age);
+age = 30;
+console.log("age2:", age);
+
+let price = 100
+console.log("price1:", price);7
+price = 200
+console.log("price2:", price);
+
+const tax = 0.1
+console.log("tax1:", tax);
+//compare in js
+//
+// if condition
+if(age > 18){
+    console.log("Adult");
+}
+
+if(age != ageAString){
+    console.log("age is greater than 18");
+}
+// == value
+// === value and type
+// != value
+// !== value and type
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to
+// && and
+// || or
